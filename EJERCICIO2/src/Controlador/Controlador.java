272b62a3c5fabc516364c22e4de370c3898cd107
miguelcarrillo-1894 <miguelcarrillo.1894@gmail.com>
2025-03-19 18:52:18 -0500

    Update Controlador.java

--- EJERCICIO2/src/Controlador/Controlador.java
+++ EJERCICIO2/src/Controlador/Controlador.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Controlador;
 
 import Modelo.Modelo;
 import Vista.Vista;
 
-/**
- *
- * @author SENA
- */
+
 public class Controlador {
       private Modelo modelo;
     private Vista vista;
