7a9b56e6668954ec33f48116124f5c407e2b7919
miguelcarrillo-1894 <miguelcarrillo.1894@gmail.com>
2025-03-19 18:57:08 -0500

    Update EJERCICIO3.java

--- EJERCICIO3/src/ejercicio3/EJERCICIO3.java
+++ EJERCICIO3/src/ejercicio3/EJERCICIO3.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejercicio3;
 
 import Controlador.Controlador;
 import Modelo.Modelo;
 import Vista.Vista;
 
-/**
- *
- * @author SENA
- */
+
 public class EJERCICIO3 {
     public static void main(String[] args) {
         Modelo modelo = new Modelo();
