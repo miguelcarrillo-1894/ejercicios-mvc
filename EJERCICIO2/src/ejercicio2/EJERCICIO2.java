e99a3f1ff66129feeff046d1a8de5246f87b70d9
miguelcarrillo-1894 <miguelcarrillo.1894@gmail.com>
2025-03-19 18:54:15 -0500

    Update EJERCICIO2.java

--- EJERCICIO2/src/ejercicio2/EJERCICIO2.java
+++ EJERCICIO2/src/ejercicio2/EJERCICIO2.java
@@ -1,22 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejercicio2;
 
 import Controlador.Controlador;
 import Modelo.Modelo;
 import Vista.Vista;
 
-/**
- *
- * @author SENA
- */
+
 public class EJERCICIO2 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
          
           Vista vista = new Vista();
