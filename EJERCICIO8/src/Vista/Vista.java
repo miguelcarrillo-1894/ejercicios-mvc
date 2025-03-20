959b11613d06f95d207e1bc48acc6c0afe370ac5
miguelcarrillo-1894 <miguelcarrillo.1894@gmail.com>
2025-03-19 19:09:27 -0500

    Update Vista.java

--- EJERCICIO8/src/Vista/Vista.java
+++ EJERCICIO8/src/Vista/Vista.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Vista;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author Usuario
- */
+
 public class Vista {
      private Scanner scanner;
 
