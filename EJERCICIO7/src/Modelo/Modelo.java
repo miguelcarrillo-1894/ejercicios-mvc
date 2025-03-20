e394a7cf897645a13ad70c55ef77e84c27eded59
miguelcarrillo-1894 <miguelcarrillo.1894@gmail.com>
2025-03-19 19:06:48 -0500

    Update Modelo.java

--- EJERCICIO7/src/Modelo/Modelo.java
+++ EJERCICIO7/src/Modelo/Modelo.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Modelo;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Usuario
- */
+
 public class Modelo {
      private boolean aireEncendido;
     private ArrayList<String> registros;
